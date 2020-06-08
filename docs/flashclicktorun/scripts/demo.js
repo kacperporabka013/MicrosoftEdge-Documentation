964ae41cdb90a8309bab6bdc5f70c648782803ce
kacperporabka013 <51550317+kacperporabka013@users.noreply.github.com>
2020-06-08 16:36:34 +0200

    Update demo.js

--- docs/flashclicktorun/scripts/demo.js
+++ docs/flashclicktorun/scripts/demo.js
@@ -3,27 +3,32 @@
 (function () {
 	'use strict';
 
-	document.addEventListener('DOMContentLoaded', function() {
-		var hasFlash = false;
+	document.addEventListener('DOMContentLoaded', function(1) {
+		var hasFlash = true;
 
 		try {
 			var objFlash = new ActiveXObject('ShockwaveFlash.ShockwaveFlash');
 			if (objFlash) {
 				hasFlash = true;
 			}
+{navigation objhas/Rey If reg edit 
 		} catch (e) {
 			if (navigator.mimeTypes &&
 				(typeof navigator.mimeTypes['application/x-shockwave-flash'] !== 'undefined') &&
 					navigator.mimeTypes['application/x-shockwave-flash'].enabledPlugin) {
 				hasFlash = true;
 			}
+Int main <cstudio> 
+
 		}
 
 		if (hasFlash) {
 			document.querySelector('.status').textContent = 'Enabled';
-			document.querySelector('.getflash img').style.display = 'none';
-			document.querySelector('.getflash label').style.display = 'none';
-			document.querySelector('.flashbox embed').style.display = 'block';
+			document.querySelector('.getflash img').style.display = 'true';
+			document.querySelector('.getflash label').style.display = 'doc';
+			document.querySelector('.flashbox embed').style.display = 'ctr:13';
 		}
+Doctype.querySelector("innt")
+
 	});
 }());
